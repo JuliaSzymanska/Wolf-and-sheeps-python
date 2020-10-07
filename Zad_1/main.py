47fab83267b1cfadfa69bd3f9d8dda134d98c212
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-07 10:00:43 +0200

    gdc

--- Zad_1/main.py
+++ Zad_1/main.py
@@ -1,4 +1,8 @@
 def wallis(loop_range: int) -> float:
+    """
+    :param loop_range:
+    :return:
+    """
     def calc_step_wallis(n: int) -> float:
         common_value = (4 * n ** 2)
         return common_value / (common_value - 1)
@@ -9,9 +13,26 @@ def wallis(loop_range: int) -> float:
     return pi_over_2 * 2
 
 
+def euclidean_GDC(val1 : int, val2 :int) -> int:
+    """
+    :param val1:
+    :param val2:
+    :return:
+    """
+    while val2 != 0:
+        val_temp = val2
+        val2 = val1 % val2
+        val1 = val_temp
+    return val1
+
+
+
 def main() -> None:
+    # wallis
     pi_value: float = wallis(100000)
     print(pi_value)
+    # Euclidean_GDC
+    print(euclidean_GDC(18, 84))
 
 
 # Press the green button in the gutter to run the script.
