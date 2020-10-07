feb7c610ed26d86cedd651659182a314a26d876b
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-07 11:09:07 +0200

    zad 1 done

--- Zad_1/main.py
+++ Zad_1/main.py
@@ -33,6 +33,7 @@ def euclidean_GDC(val1: int, val2: int) -> int:
     return val1
 
 
+# todo, check if val >= 2
 def sieve_of_erastothenes(max_val: int) -> list:
     """
     :param max_val:
@@ -48,6 +49,37 @@ def sieve_of_erastothenes(max_val: int) -> list:
     return values
 
 
+# todo cleanup
+def least_common_multiple(val1: int, val2: int) -> int:
+    val1_divisors: list = []
+    while val1 > 1:
+        possible_values = list(range(2, val1 + 1))
+        for i in possible_values:
+            if val1 % i == 0:
+                val1 = int(val1 / i)
+                val1_divisors.append(i)
+                break
+    val2_divisors: list = []
+    while val2 > 1:
+        possible_values = list(range(2, val2 + 1))
+        for i in possible_values:
+            if val2 % i == 0:
+                val2 = int(val2 / i)
+                val2_divisors.append(i)
+                break
+    val1_divisors_unique = set(val1_divisors)
+    val2_divisors_unique = set(val2_divisors)
+    least_common_multiple_value: int = 1
+    for i in val1_divisors_unique:
+        count = max(val1_divisors.count(i), val2_divisors.count(i))
+        least_common_multiple_value *= i ** count
+        val2_divisors_unique.remove(i)
+    print(val2_divisors_unique)
+    for i in val2_divisors_unique:
+        least_common_multiple_value *= i ** val2_divisors.count(i)
+    return least_common_multiple_value
+
+
 def main() -> None:
     # wallis
     for i in range(1, 10):
@@ -55,7 +87,9 @@ def main() -> None:
     # Euclidean_GDC
     print(euclidean_GDC(18, 84))
     # Sieve
-    print(sieve_of_erastothenes(100))
+    print(sieve_of_erastothenes(64))
+    # LCM
+    print(least_common_multiple(192, 348))
 
 
 # Press the green button in the gutter to run the script.
