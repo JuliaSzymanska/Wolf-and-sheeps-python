19a2ebdc7a319b88314312cacbd3a6f6c76e7554
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-07 10:06:06 +0200

    wallis comment

--- Zad_1/main.py
+++ Zad_1/main.py
@@ -1,19 +1,26 @@
+# todo only positive integers
+# todo test with online values
 def wallis(loop_range: int) -> float:
     """
     :param loop_range:
     :return:
     """
+
     def calc_step_wallis(n: int) -> float:
         common_value = (4 * n ** 2)
         return common_value / (common_value - 1)
-    pi_over_2: float = calc_step_wallis(1)
-    for i in range(2, loop_range):
+
+    pi_over_2: float = 0
+    for i in range(1, loop_range + 1):
         val = calc_step_wallis(i)
-        pi_over_2 *= val
+        if i == 1:
+            pi_over_2 = val
+        else:
+            pi_over_2 *= val
     return pi_over_2 * 2
 
 
-def euclidean_GDC(val1 : int, val2 :int) -> int:
+def euclidean_GDC(val1: int, val2: int) -> int:
     """
     :param val1:
     :param val2:
@@ -26,11 +33,10 @@ def euclidean_GDC(val1 : int, val2 :int) -> int:
     return val1
 
 
-
 def main() -> None:
     # wallis
-    pi_value: float = wallis(100000)
-    print(pi_value)
+    for i in range(1, 10):
+        print(wallis(i))
     # Euclidean_GDC
     print(euclidean_GDC(18, 84))
 
