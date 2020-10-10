455998eec3debf4110a27937678514952b99ad27
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-10 11:40:55 +0200

    Sheep moves work

--- Zad_2/main.py
+++ Zad_2/main.py
@@ -17,9 +17,11 @@ class Sheep(Animal):
     def __init__(self, init_pos_limit, move_dist):
         super().__init__(move_dist)
         self.init_pos_limit: float = init_pos_limit
+        self.init_position()
 
     def init_position(self):
         self.position[0]: float = random.uniform(-self.init_pos_limit, self.init_pos_limit)
+        self.position[1]: float = random.uniform(-self.init_pos_limit, self.init_pos_limit)
 
     def select_move(self) -> str:
         moves: [str] = ["east", "west", "north", "south"]
@@ -27,7 +29,18 @@ class Sheep(Animal):
 
     def move(self):
         selected_move: str = self.select_move()
-
+        if selected_move == "east":
+            self.position[0] += self.move_dist
+            return
+        elif selected_move == "west":
+            self.position[0] -= self.move_dist
+            return
+        elif selected_move == "north":
+            self.position[1] += self.move_dist
+            return
+        elif selected_move == "south":
+            self.position[1] -= self.move_dist
+            return
 
 
 class Wolf(Animal):
@@ -36,4 +49,7 @@ class Wolf(Animal):
 
 
 if __name__ == '__main__':
-    print(random.uniform(1, 20))
+    sheep = Sheep(10.0, 0.5)
+    print(sheep.position)
+    sheep.move()
+    print(sheep.position)
