324c91bee9173ce4e94a0304172f50c11beca18d
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-10 11:31:48 +0200

    Methods for move

--- Zad_2/main.py
+++ Zad_2/main.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Animal:
     def __init__(self, move_dist):
         self.move_dist: float = move_dist
@@ -6,11 +9,21 @@ class Animal:
     def move(self):
         pass
 
+    def select_move(self):
+        moves = ["east", "west", "north", "south"]
+        return random.choice(moves)
+
 
 class Sheep(Animal):
     def __init__(self, init_pos_limit, move_dist):
         super().__init__(move_dist)
         self.init_pos_limit: float = init_pos_limit
+
+    def init_position(self):
+        self.position[0] = random.uniform(-self.init_pos_limit, self.init_pos_limit)
+
+    def move(self):
+        selected_move = self.select_move()
 
 
 class Wolf(Animal):
@@ -19,5 +32,4 @@ class Wolf(Animal):
 
 
 if __name__ == '__main__':
-    print("Hi")
-    a: float = 3
+    print(random.uniform(1, 20))
