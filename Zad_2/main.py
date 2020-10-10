e9a2d158d80bcad5fc71f08c4ad36a99b64d6f74
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-10 20:18:46 +0200

    Wolf makes moves

--- Zad_2/main.py
+++ Zad_2/main.py
@@ -10,15 +10,13 @@ class Animal:
     def move(self):
         pass
 
-    def select_move(self):
-        pass
-
 
 class Sheep(Animal):
     def __init__(self, init_pos_limit, move_dist):
         super().__init__(move_dist)
         self.init_pos_limit: float = init_pos_limit
         self.init_position()
+        self.is_alive = True
 
     def init_position(self):
         self.position[0]: float = random.uniform(-self.init_pos_limit, self.init_pos_limit)
@@ -43,6 +41,9 @@ class Sheep(Animal):
             self.position[1] -= self.move_dist
             return
 
+    def die(self):
+        self.is_alive = False
+
 
 class Wolf(Animal):
     def __init__(self, move_dist, game_sheep):
@@ -53,22 +54,39 @@ class Wolf(Animal):
         return math.sqrt(int(one_game_sheep.position[0] - self.position[0]) ^ 2 + int(
             one_game_sheep.position[1] - self.position[1]) ^ 2)
 
-    def select_move(self):
+    def move(self):
         distance = []
         for s in self.game_sheep:
-            distance.append(self.calculate_distance(s))
+            if s.is_alive:
+                distance.append(self.calculate_distance(s))
+            else:
+                distance.append(None)
 
+        index = distance.index(min(distance))
+        if distance[index] < self.move_dist:
+            self.game_sheep[index].die()
+            return
+        x_diff = self.position[0] - self.game_sheep[index].position[0]
+        y_diff = self.position[1] - self.game_sheep[index].position[1]
+        if math.fabs(x_diff) <= y_diff:
+            if x_diff < 0:
+                self.position[0] += self.move_dist
+            else:
+                self.position[0] -= self.move_dist
+        else:
+            if y_diff < 0:
+                self.position[1] += self.move_dist
+            else:
+                self.position[1] -= self.move_dist
+
+def perform_simulation(rounds: int, number_of_sheep: int, init_pos_limit: float, sheep_move_dist: float, wolf_move_dist: float):
+    sheep: [Sheep] = []
+    for i in range(number_of_sheep):
+        sheep.append([Sheep(init_pos_limit, sheep_move_dist)])
+    wolf: Wolf = Wolf(wolf_move_dist, sheep)
+    for i in range(rounds):
 
 
 
 if __name__ == '__main__':
-    sheep: [Sheep] = []
-    for i in range(10):
-        sheep.append([Sheep(10.0, 0.5)])
-
-    # wolf: Wolf = Wolf(1.0, sheep)
-    # print("Sh", sheep)
-    # wolf.select_move()
-    # sheep.move()
-    # print("Sh", sheep)
-    # wolf.select_move()
+    perform_simulation(50, 15, 10.0, 0.5, 1.0)
