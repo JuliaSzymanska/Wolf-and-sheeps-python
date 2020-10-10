2d68f18ace8bfc4ef506895c8b5aea5fff0b8364
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-10 11:18:23 +0200

    Added classes

--- Zad_2/main.py
+++ Zad_2/main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+class Animal:
+    def __init__(self, move_dist):
+        self.move_dist: float = move_dist
+        self.position: [float] = [0.0, 0.0]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def move(self):
+        pass
 
 
-# Press the green button in the gutter to run the script.
+class Sheep(Animal):
+    def __init__(self, init_pos_limit, move_dist):
+        super().__init__(move_dist)
+        self.init_pos_limit: float = init_pos_limit
+
+
+class Wolf(Animal):
+    def __init__(self, move_dist):
+        super().__init__(move_dist)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Hi")
+    a: float = 3
