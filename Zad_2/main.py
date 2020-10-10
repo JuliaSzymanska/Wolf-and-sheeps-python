e292746df23e649181108884a6714b11c52f443a
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-10 21:02:23 +0200

    Working simulation

--- Zad_2/main.py
+++ Zad_2/main.py
@@ -1,5 +1,6 @@
 import math
 import random
+from scipy.spatial import distance
 
 
 class Animal:
@@ -27,19 +28,20 @@ class Sheep(Animal):
         return random.choice(moves)
 
     def move(self):
-        selected_move: str = self.select_move()
-        if selected_move == "east":
-            self.position[0] += self.move_dist
-            return
-        elif selected_move == "west":
-            self.position[0] -= self.move_dist
-            return
-        elif selected_move == "north":
-            self.position[1] += self.move_dist
-            return
-        elif selected_move == "south":
-            self.position[1] -= self.move_dist
-            return
+        if self.is_alive:
+            selected_move: str = self.select_move()
+            if selected_move == "east":
+                self.position[0] += self.move_dist
+                return
+            elif selected_move == "west":
+                self.position[0] -= self.move_dist
+                return
+            elif selected_move == "north":
+                self.position[1] += self.move_dist
+                return
+            elif selected_move == "south":
+                self.position[1] -= self.move_dist
+                return
 
     def die(self):
         self.is_alive = False
@@ -51,23 +53,24 @@ class Wolf(Animal):
         self.game_sheep: [Sheep] = game_sheep
 
     def calculate_distance(self, one_game_sheep: Sheep):
-        return math.sqrt(int(one_game_sheep.position[0] - self.position[0]) ^ 2 + int(
-            one_game_sheep.position[1] - self.position[1]) ^ 2)
+        return distance.euclidean([self.position], [one_game_sheep.position])
+        # return math.sqrt((int(one_game_sheep.position[0] - self.position[0])) ^ 2 + (int(
+        #     one_game_sheep.position[1] - self.position[1])) ^ 2)
 
     def move(self):
-        distance = []
+        distance_to_sheep = [[], []]
         for s in self.game_sheep:
             if s.is_alive:
-                distance.append(self.calculate_distance(s))
-            else:
-                distance.append(None)
+                distance_to_sheep[0].append(self.calculate_distance(s))
+                distance_to_sheep[1].append(self.game_sheep.index(s))
 
-        index = distance.index(min(distance))
-        if distance[index] < self.move_dist:
-            self.game_sheep[index].die()
+        sheep_index = distance_to_sheep[1][distance_to_sheep[0].index(min(distance_to_sheep[0]))]
+        distance_index = distance_to_sheep[0].index(min(distance_to_sheep[0]))
+        if distance_to_sheep[0][distance_index] < self.move_dist:
+            self.game_sheep[sheep_index].die()
             return
-        x_diff = self.position[0] - self.game_sheep[index].position[0]
-        y_diff = self.position[1] - self.game_sheep[index].position[1]
+        x_diff = self.position[0] - self.game_sheep[sheep_index].position[0]
+        y_diff = self.position[1] - self.game_sheep[sheep_index].position[1]
         if math.fabs(x_diff) <= y_diff:
             if x_diff < 0:
                 self.position[0] += self.move_dist
@@ -79,13 +82,37 @@ class Wolf(Animal):
             else:
                 self.position[1] -= self.move_dist
 
-def perform_simulation(rounds: int, number_of_sheep: int, init_pos_limit: float, sheep_move_dist: float, wolf_move_dist: float):
+
+def show_information(round_number: int, wolf: Wolf, sheep):
+    print("Round number: ", round_number)
+    print("Wolf position: (", round(wolf.position[0], 3), ", ", round(wolf.position[1], 3), ")")
+    number_of_alive: int = 0
+    for s in sheep:
+        if s.is_alive:
+            number_of_alive += 1
+    print("Number of alive sheep: ", number_of_alive)
+    print("")
+
+
+def perform_simulation(rounds: int, number_of_sheep: int, init_pos_limit: float, sheep_move_dist: float,
+                       wolf_move_dist: float):
     sheep: [Sheep] = []
     for i in range(number_of_sheep):
-        sheep.append([Sheep(init_pos_limit, sheep_move_dist)])
+        sheep.append(Sheep(init_pos_limit, sheep_move_dist))
     wolf: Wolf = Wolf(wolf_move_dist, sheep)
-    for i in range(rounds):
-
+    i: int = 0
+    is_anyone_alive: int = True
+    show_information(i, wolf, sheep)
+    while i < rounds and is_anyone_alive:
+        for j in sheep:
+            j.move()
+        wolf.move()
+        i += 1
+        is_anyone_alive = False
+        for k in sheep:
+            if k.is_alive:
+                is_anyone_alive = True
+        show_information(i, wolf, sheep)
 
 
 if __name__ == '__main__':
