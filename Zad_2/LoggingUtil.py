e7ab12cc202e15570bbab231d9f01506256763f3
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-21 23:32:05 +0200

    change formatting

--- Zad_2/LoggingUtil.py
+++ Zad_2/LoggingUtil.py
@@ -10,8 +10,8 @@ def monitor_results(func):
     @functools.wraps(func)
     def wrapper(*func_args, **func_kwargs):
         retval = func(*func_args, **func_kwargs)
-        logging.getLogger(__name__).debug(
-            'function ' + func.__name__ + '() params ' + func_args.__str__() + ' returns ' + repr(retval))
+        logging.getLogger(__name__).debug('function %s () params %s returns %s', func.__name__, func_args.__str__(),
+                                          repr(retval))
         return retval
 
     return wrapper
