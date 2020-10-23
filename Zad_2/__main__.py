8f7d76c3396899871bbbbfd0d1bc6c1f3db7af3b
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-23 18:55:02 +0200

    remove no need

--- Zad_2/__main__.py
+++
@@ -1,8 +0,0 @@
-from chase import Commandline, Simulation, Config
-
-if __name__ == '__main__':
-    parser = Commandline.init_argparse()
-    parser.parse_args()
-    simulation = Simulation(Config.ROUNDS, Config.SHEEP, Config.INIT_POS_LIMIT, Config.SHEEP_MOVE_DIST,
-                            Config.WOLF_MOVE_DIST)
-    simulation.perform_simulation()
