130acc76a5e0917ea5496b346907937d70467d24
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-28 10:19:51 +0100

    xd

--- Zad_2/setup.py
+++ Zad_2/setup.py
@@ -1,12 +1,5 @@
 from distutils.core import setup
 
-import os
-thelibFolder = os.path.dirname(os.path.realpath(__file__))
-requirementPath = thelibFolder + '/requirements.txt'
-install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
-if os.path.isfile(requirementPath):
-    with open(requirementPath) as f:
-        install_requires = f.read().splitlines()
 setup(
     name='chase',
     version='1.3',
